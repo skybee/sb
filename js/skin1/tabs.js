3958904fc9d5fd4d01cc3b25559f5b8949689611
sb <sb@sb-pc>
2014-02-13 16:32:17 +0200

    bg & slider rotate fasted

--- js/skin1/tabs.js
+++ js/skin1/tabs.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-	$('#londontabs > ul').tabs({ fx: { height: 'toggle', opacity: 'toggle' } });
-	$('#tabsheader > ul').tabs({fx: {height:'toggle', opacity:'toggle'}});
-	$('#featuredvid > ul').tabs();
-});
+//$(document).ready(function() {
+//	$('#londontabs > ul').tabs({ fx: { height: 'toggle', opacity: 'toggle' } });
+//	$('#tabsheader > ul').tabs({fx: {height:'toggle', opacity:'toggle'}});
+//	$('#featuredvid > ul').tabs();
+//});
 	$(document).ready(function(){
-		$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 2800, false);
+		$("#featured > ul").tabs( {fx:{opacity: "toggle", duration: 200}} ).tabs("rotate", 2800, false );
 	});
 	
 /*window.onload = function() {$('#Picture').hide().fadeIn(3000);};*/
