6831b25809ba522931f8ec5e238c6cb8158e5526
sb_note <skybee84@yandex.ru>
2014-02-28 19:43:40 +0200

    left last news

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -16,18 +16,26 @@ class main extends CI_Controller {
     function index(){ $this->main_page('news'); }
 
     function main_page($cat_name) {
+        $this->benchmark->mark('1');
         $data_ar['main_cat_ar']         = $this->article_m->get_cat_data_from_url_name($cat_name);
+        $this->benchmark->mark('2');
         $data_ar['main_menu_list']      = $this->list_m->get_cat(0);
+        $this->benchmark->mark('3');
         $data_ar['second_menu_list']    = $this->list_m->get_cat($data_ar['main_cat_ar']['id']);
+        $this->benchmark->mark('4');
         $data_ar['mainpage_cat_list']   = $this->article_m->get_mainpage_cat_news($data_ar['second_menu_list']);
+        $this->benchmark->mark('5');
         $data_ar['meta']['title']       = $data_ar['main_cat_ar']['title'];
         
+        $this->benchmark->mark('6');
         $top_slider['articles']         = $this->article_m->get_popular_articles(1, 8, 5, 0, 350);
         $top_slider['main_cat_url']     = $data_ar['main_cat_ar']['url_name'];
         
+        $this->benchmark->mark('7');
         $last_news['last_news']         = $this->article_m->get_last_news( $data_ar['main_cat_ar']['id'], 50, false, true );
 //        echo '<pre>'.print_r($top_slider,1).'</pre>';
 
+        $this->benchmark->mark('8');
         $tpl_ar = $data_ar; //== !!! tmp    
         $tpl_ar['content']  = $this->load->view('component/main_latest_v', $data_ar, true);
         $tpl_ar['content'] .= $this->load->view('component/cat_listing_v', $data_ar, true);
@@ -37,6 +45,25 @@ class main extends CI_Controller {
         $tpl_ar['top_slider']   = $this->load->view('component/slider_top_v', $top_slider, true);
 
         $this->load->view('main_v', $tpl_ar);
+        
+        $this->benchmark->mark('end');
+        
+        echo    '<br /> 1 - ';
+        echo  $this->benchmark->elapsed_time('1', '2');
+        echo    '<br /> 2 - ';
+        echo  $this->benchmark->elapsed_time('2', '3');
+        echo    '<br /> 3 - ';
+        echo  $this->benchmark->elapsed_time('3', '4');
+        echo    '<br /> 4 - ';
+        echo  $this->benchmark->elapsed_time('4', '5');
+        echo    '<br /> 5 - ';
+        echo  $this->benchmark->elapsed_time('5', '6');
+        echo    '<br /> 6 - ';
+        echo  $this->benchmark->elapsed_time('6', '7');
+        echo    '<br /> 7 - ';
+        echo  $this->benchmark->elapsed_time('7', '8');
+        echo    '<br /> 8 - ';
+        echo  $this->benchmark->elapsed_time('8', 'end');
     }
 
     function document($cat_name, $s_cat_name, $url_id_name) {
--- application/models/article_m.php
+++ application/models/article_m.php
@@ -19,26 +19,30 @@ class article_m extends CI_Model{
         if( $img )  $img_sql = " AND article.main_img != '' ";
         else        $img_sql = "";
             
+        $sql = "    SELECT 
+                        article.id, article.date, article.url_name, article.title, article.main_img,
+                        category1.id AS 's_cat_id', category1.url_name AS 's_cat_uname', category1.name AS 's_cat_name',
+                        category2.id AS 'f_cat_id', category2.url_name AS 'f_cat_uname'
+                    FROM 
+                        `category` AS `category1`,
+                        `category` AS `category2`,
+                        `article`
+                    WHERE
+                        -- article.cat_id  = '$ cat'
+                        (`cat_id` = '{$idParentId}' OR `cat_id` IN (SELECT `id` FROM `category` WHERE `parent_id` = '{$idParentId}') )    
+                    AND
+                        category1.id    = article.cat_id
+                    AND
+                        category2.id    = category1.parent_id
+                        {$img_sql}
+                    ORDER BY article.date DESC
+                    LIMIT {$cnt}   
+              ";
         
-        $query = $this->db->query(" SELECT 
-                                        article.id, article.date, article.url_name, article.title, article.main_img,
-                                        category1.id AS 's_cat_id', category1.url_name AS 's_cat_uname', category1.name AS 's_cat_name',
-                                        category2.id AS 'f_cat_id', category2.url_name AS 'f_cat_uname'
-                                    FROM 
-                                        `category` AS `category1`,
-                                        `category` AS `category2`,
-                                        `article`
-                                    WHERE
-                                        -- article.cat_id  = '$ cat'
-                                        (`cat_id` = '{$idParentId}' OR `cat_id` IN (SELECT `id` FROM `category` WHERE `parent_id` = '{$idParentId}') )    
-                                        AND
-                                        category1.id    = article.cat_id
-                                        AND
-                                        category2.id    = category1.parent_id
-                                        {$img_sql}
-                                    ORDER BY article.date DESC
-                                    LIMIT {$cnt}   
-                                  ");
+        
+        $query = $this->db->query($sql);
+        
+//        echo "<pre>{$sql}</pre><br /><br />";
                                     
         $result_ar = array();
         foreach( $query->result_array() as $row ){
