2f08daa9146bc20f2781469f5429cbb573ef2df1
skybee <skybee84@ya.ru>
2014-10-23 13:02:22 +0300

    price.ua

--- application/controllers/donor_domain/domain.php
+++ application/controllers/donor_domain/domain.php
@@ -1,5 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+exit();
 
 set_time_limit(3600*5);
 
--- application/views/component/main_other_news_v.php
+++ application/views/component/main_other_news_v.php
@@ -1,9 +1,28 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<div class="othernews">
-    <div class="listing other_news_listing">
-        <div class="header">
-            <h1>ИНТЕРЕСНОЕ ИЗ ДРУГИХ РАЗДЕЛОВ</h1>
-        </div><!-- #header -->
-    </div><!-- #listing -->
-</div><!-- #othernews -->
+<div class="cat_listing">
+
+    <div class="header">
+        <h1>
+            ИНТЕРЕСНОЕ ИЗ ДРУГИХ РАЗДЕЛОВ
+        </h1>
+    </div><!-- #header -->
+
+<!--    <div class="content">
+        <div class="left">
+                <div class="imgholder">
+                    <a href="/hi-tech/pc-hardware/-192247-svet-i-ten-v-ofise/">
+                        <img src="/upload/images/medium/2014/10/15/1354812306_1421.jpg" alt="Свет и тень в офисе" border="0" onerror="imgError(this);" />
+                    </a>
+                    <div class="description"><h3><a href="/hi-tech/pc-hardware/-192247-svet-i-ten-v-ofise/">Свет и тень в офисе</a></h3></div> #description 
+                </div> #imgholder 
+            </div> #left 
+
+
+        <div class="right">
+        </div> #right 
+    </div> #content -->
+</div>
+
+
+
