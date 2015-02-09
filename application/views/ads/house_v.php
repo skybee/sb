ba931232751ab5167102ca49900d44ad709718cd
skybee <skybee84@ya.ru>
2015-02-09 19:14:50 +0200

    HC ADS

---
+++ application/views/ads/house_v.php
@@ -0,0 +1,9 @@
+<div class="hc250x600">
+    <a href="http://house-control.org.ua/" target="_blank">
+        <img src="/img/hc/hc-250x600.png" alt="Видеонаблюдение | Домофоны | Сигнализация - House Control Украина" /> 
+        <div class="house-hover house-hover-1"></div>
+        <div class="house-hover house-hover-2"></div>
+        <div class="house-hover house-hover-3"></div>
+    </a>
+</div>
+
