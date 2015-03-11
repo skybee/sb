73f2aec016efcc9cad2b2b11948f31d0f9d2701c
skybee <skybee84@ya.ru>
2015-03-11 23:24:07 +0200

    serp scan

---
+++ application/controllers/cron/serp_parse.php
@@ -0,0 +1,73 @@
+<?php
+
+set_time_limit(60);
+
+class serp_parse extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->load->database();
+        $this->load->model('serp_parse_m');
+        $this->load->library('parser/serp_parse_lib');
+    }
+
+    function yandex_xml($cnt_scan = 1)
+    {
+        header("Content-type:text/plain; Charset=utf-8");
+
+        if( $this->single_work( 2, 'upd_article_view') == false ){
+            exit('The work temporary Lock');
+        }
+
+        $cnt_scan = (int) $cnt_scan;
+        
+        $articlesList = $this->serp_parse_m->getArticles($cnt_scan);
+
+        if(!$articlesList){
+            exit('No Articles');
+        }
+
+        $this->serp_parse_lib->setThisHost('odnako.su');
+
+        foreach($articlesList as $articleData)
+        {
+            $serpData = $this->serp_parse_lib->getData($articleData);
+            if(!$serpData){
+                echo "Parse Error: ID-".$articleData['id'].' '.$articleData['title']."\n\n";
+                continue;
+            }
+            $jsonData = json_encode($serpData);
+
+            if( $this->serp_parse_m->updateSerpData($articleData['id'], $jsonData) ){
+                echo "OK: ID-".$articleData['id'].' '.$articleData['title']."\n\n";
+            }
+            else{
+                echo "Update SQL Error: ID-".$articleData['id'].' '.$articleData['title']."\n\n";
+            }
+            
+            flush();
+            sleep(1);
+        }
+    }
+
+    private function single_work( $minutes, $fname = 'null' ){
+        $lockFile   = 'lock/'.$fname.'.lock';
+        $lockTime   = time() + (60*$minutes);
+
+
+        if( is_file($lockFile) ){
+            $fileTimeLock   = file_get_contents($lockFile);
+            $fileTimeLock   = (int) $fileTimeLock;
+
+            if( time() < $fileTimeLock ) return FALSE;
+        }
+
+        file_put_contents($lockFile, $lockTime );
+
+        return TRUE;
+    }
+}
+
---
+++ application/libraries/parser/serp_parse_lib.php
@@ -0,0 +1,120 @@
+<?php
+
+
+class serp_parse_lib
+{
+    private $yandexUrl  = 'https://xmlsearch.yandex.com/xmlsearch?user=skybee84&key=03.47372985:0ec36ca944189961ac6da27d41f5d404&maxpassages=5&groupby=groups-on-page%3D20';
+    private $thisHost   = false;
+
+    function setThisHost($thisHost)
+    {
+        $this->thisHost = $thisHost;
+    }
+
+    function getData($articleData)
+    {
+        $title      = $this->cleanQuery($articleData['title']);
+        $queryUrl   = $this->yandexUrl .'&query='.$title;
+        $serpXmlStr = $this->down_with_curl($queryUrl);
+        $serpXmlStr = $this->cleanXmlStr($serpXmlStr);
+
+        $xmlObj     = simplexml_load_string($serpXmlStr);
+        if($xmlObj === false){
+            return false;
+        }
+
+        $data = $this->getDataFromXml($xmlObj);
+
+        $data = $this->delThisHostFromResult($data, $this->thisHost);
+
+        return $data;
+    }
+
+    private function getDataFromXml($xml)
+    {
+        if( isset($xml->response->error) ) {
+            $errorMsg = "Serp Parse Error: ".$xml->response->error;
+            log_message('error', $errorMsg);
+
+            return false;
+        }
+
+        $results = $xml->response->results->grouping->group;
+
+        $data = array();
+        foreach($results as $row){
+            $tmpRow['title']    = (string) $row->doc->title;
+            $tmpRow['host']     = (string) $row->doc->domain;
+            $tmpRow['url']      = (string) $row->doc->url;
+
+            $tmpRow['text'] = '';
+            
+            if(isset($row->doc->headline)){
+                $tmpRow['text'] .= (string) $row->doc->headline;
+            }
+            if(isset($row->doc->passages)){
+                $tmpRow['text'] .= (string) $row->doc->passages;
+            }
+
+            $data[] = $tmpRow;
+        }
+
+        return $data;
+    }
+
+    private function delThisHostFromResult($results, $thisHost = false)
+    {
+        if(!$thisHost){
+            $thisHost = $_SERVER['HTTP_HOST'];
+        }
+
+        if(count($results) < 1){
+            return $results;
+        }
+
+        $newResults = array();
+        foreach($results as $res){
+            if($res['host'] == $thisHost){
+                continue;
+            }
+
+            $newResults[] = $res;
+        }
+
+        return $newResults;
+    }
+
+    private function cleanXmlStr($str)
+    {
+        $pattern = "/<\/?(hlword|passage)>/";
+        $str = preg_replace($pattern, ' ', $str);
+        $str = str_replace('...', ' ', $str);
+        return $str;
+    }
+
+    private function cleanQuery($query)
+    {
+        $query = html_entity_decode($query);
+        $query = urlencode($query);
+        return $query;
+    }
+
+    private function down_with_curl($url)
+    {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko' );
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_ENCODING, '');
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
+
+	$content = curl_exec($ch);
+	curl_close($ch);
+
+	return $content;
+    }
+}
+
